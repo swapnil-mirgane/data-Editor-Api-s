8cae8a93f1c2c59c46f1a7e24c2f55071e3888b4
rainvikas <vikasyadav.vy204@gmail.com>
2022-10-18 15:45:01 +0530

    feat: updated getByQuery api

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,42 +3,39 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { sample_data } from './entities/sample.entity';
 
-
 @Injectable()
 export class AppService {
-  constructor (
+  constructor(
     @InjectRepository(sample_data)
-    private readonly sample_dataRepository: Repository<sample_data>
-     ) {}
+    private readonly sample_dataRepository: Repository<sample_data>,
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }
 
-  // find by id  
+  // find by id
   async findOne(id: number) {
-    let data:any = await this.sample_dataRepository.findOneBy({id});  
-    let baseUrl=process.env.baseUrl
-    console.log(data);
-    
-    if(!data){
-      return{
-        msg:`No Data Found `
-      }
+    let data: any = await this.sample_dataRepository.findOneBy({ id });
+    let baseUrl = process.env.baseUrl;
+
+    if (!data) {
+      return {
+        msg: `No Data Found `,
+      };
     }
-    let url=` ${baseUrl}/storage/${data.filename}.JPG `
+    let url = ` ${baseUrl}/storage/${data.fname}`;
     return {
-      data : { url }
-    }
+      data: { url },
+    };
   }
 
-// find by bounding box
   async find(BBox) {
     let data1: any = await this.sample_dataRepository
       .query(`SELECT * FROM sample_data WHERE ST_Contains(
-          ST_Transform(
-              ST_MakeEnvelope(${BBox},4326)
-              ,4326)
-          ,sample_data.geom::geometry)`);
+        ST_Transform(
+            ST_MakeEnvelope(${BBox},4326)  
+            ,4326)
+        ,sample_data.geom::geometry)`);
     let data = await data1.map((ele) => {
       return {
         id: ele.id,
@@ -46,8 +43,6 @@ export class AppService {
         long: ele.long,
       };
     });
-    return { data };
+    return { NoOfData: data.length, data };
   }
-
-
 }
--- src/entities/sample.entity.ts
+++ src/entities/sample.entity.ts
@@ -3,27 +3,31 @@ import { Column, CreateDateColumn, Entity,PrimaryGeneratedColumn } from "typeorm
 import { Geometry } from 'geojson';
 @Entity('sample_data')
 export class sample_data {
-    @PrimaryGeneratedColumn()
-    id: number;
-    @Column({type:'decimal',nullable:true})
-    lat:string;
-    @Column({type:'decimal',nullable:true})
-    long:string;
-    @Column({type:'decimal',nullable:true})
-    alt:number;
-    @Column()
-    filename:string;
-    @Column({type:'jsonb',default:{}})
-    raw_data:any
-    @CreateDateColumn()
-    created_date:Date;
-    @Column({
-        type: 'geometry',
-        spatialFeatureType: 'geometry',
-        srid: 4326,
-        nullable: true,
-      })
-      geom: Geometry;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'decimal', nullable: true })
+  lat: any;
+
+  @Column({ type: 'decimal', nullable: true })
+  long: any;
+
+  @Column({ type: 'decimal', nullable: true })
+  alt: any;
+
+  @Column({ nullable: true })
+  fname: string;
+
+  @Column({
+    type: 'geometry',
+    spatialFeatureType: 'geometry',
+    srid: 4326,
+    nullable: true,
+  })
+  geom: Geometry;
+
+  @Column({ type: 'timestamptz' }) 
+  timestamp: Date;
 }
     
     
